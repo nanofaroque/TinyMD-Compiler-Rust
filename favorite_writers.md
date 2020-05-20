6fad7d88cf01a07a528916d21de3ba006001edea
ofaroque <ofaroque@amazon.com>
2020-05-19 22:28:44 -0500

    incomplete work

---
+++ favorite_writers.md
@@ -0,0 +1,16 @@
+# My favorite author
+
+This is a report about my favorite writer. His name is Omar Faroque.
+
+# Jesse's favorite food
+
+Jesse really likes enchiladas and any kind of sushi. 
+
+# Omar's favorite drink
+
+Jesse likes to drink coffee in the mornings and iced tea throughout the day. Sometimes, he even drinks water. 
+
+# Omar's favorite hobbies
+
+Omar likes to write about computer programming and game design, and when he is not hunched over a computer, you can find him out on a run and listening to a podcast or the serenity of mother nature. 
+ 
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
+use std::path::Path;
+use std::fs::File;
+use std::error::Error;
+use std::io::BufReader;
+
+
 fn main() {
     usage();
     println!("{}", get_title());
     print_long_banner();
+
+    let args: Vec<String> = std::env::args().collect();
+    match args.len() { //match is like switch case
+        2 => parse_markdown_file(&args[1]), // for 2 do this
+        _ => {
+            println!("[ ERROR ] Invalid invocation (you done goofed!)");
+            usage();
+        } // otherwise do usage()
+    }
 }
 
 fn get_title() -> String {
@@ -13,7 +28,28 @@ fn get_title() -> String {
     return the_title;
 }
 
-fn parse_markdown_file() {}
+fn parse_markdown_file(_filename: &str) {
+    print_short_banner();
+    print_short_banner();
+    println!("[ INFO ] Starting parser!");
+
+    // Create a path variable from the filename
+    let input_filename = Path::new(_filename);
+
+    // Try to open the file
+    let file = File::open(&input_filename)
+        .expect("[ ERROR ] Failed to open file!");
+
+    let mut _ptag: bool = false;
+    let mut _htag: bool = false;
+
+    // Create a place to store all our tokens
+    let mut tokens: Vec<String> = Vec::new();
+
+    // Read the file line-by-line
+    let reader = BufReader::new(file);
+
+}
 
 fn print_short_banner() {
     println!("{}", get_title());
